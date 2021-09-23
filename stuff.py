74eec814efda871967ef6c6383b9d272f12f16d0
benlittell <blittell13@gmail.com>
2021-09-23 14:49:32 -0400

    problems 1 and 2

--- stuff.py
+++ stuff.py
@@ -1,1 +1,21 @@
-print('hello')
+def write_file(filename, a_string):
+    with open(filename, 'a') as file:
+        file.write(a_string)
+
+
+# write_file("test.txt", 'Test\n')
+
+
+def open_file(filename, mode='read'):
+        with open(filename, 'r') as file:
+            if mode == 'read':
+                text = file.read()
+            elif mode == 'readlines':
+                text = file.readlines()
+            elif mode == 'readline':
+                text = file.readline()
+            return text
+
+
+# open_ = open_file('test.txt', 'readlines')
+
